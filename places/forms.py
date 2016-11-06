8e1f18ed84bbc4433e5a38d3b966d33e238bb117
Elif Sezgin <elifsezgin@Sezers-MacBook-Pro.local>
2016-11-06 01:51:00 -0800

    first commit

---
+++ places/forms.py
@@ -0,0 +1,51 @@
+from django.forms import (
+	ModelForm, HiddenInput, ValidationError
+)
+
+from places.models import Place, Media, Review
+
+class PlaceCreationForm(ModelForm):
+	class Meta:
+		model = Place
+		fields = (
+			'name',
+			'coordinates',
+			'category',
+			'has_wifi',
+			'telephone',
+			'description',
+		)
+		widgets = {
+			'coordinates': HiddenInput
+		}
+
+	def clean_coordinates(self):
+		coords = self.cleaned_data['coordinates']
+
+		try:
+			lat, lng = coords.split(',')
+			lat = float(lat)
+			lng = float(lng)
+
+			if (
+				lat < -90 or lat > 90 or
+				lng < -180 or lng > 180
+			):
+				raise ValidationError('Please enter a valid coordinate')
+
+		except ValueError:
+			raise ValidationError('Enter the coordinates')
+
+		return coords
+
+
+class MediaCreationForm(ModelForm):
+	class Meta:
+		model = Media
+		fields = ('image', )
+
+
+class ReviewCreationForm(ModelForm):
+	class Meta:
+		model = Review
+		fields = ('comment', 'vote')
